2e266b27651c1e59bcb7a34180d3dd40c3743d60
anton <astruyk@gmail.com>
2014-07-29 20:54:41 -0400

    Update webpage
    Now that this is published, it's an actual webpage!

--- src/mod.cpp
+++ src/mod.cpp
@@ -1,5 +1,5 @@
 name="Ares - Curator Improvements";
 picture="ares_logo.paa";
-action="https://github.com/";
+action="https://github.com/astruyk/Ares";
 actionName="Website";
 description = "Add some extra functionality to Zeus.";
