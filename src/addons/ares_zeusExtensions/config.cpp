77ec66bea163d0fff0edd237a87c2b974d6b89af
Anton <Anton@.(none)>
2014-08-20 09:10:00 -0400

    Attempt to get a module to show up. Failing so far.

--- src/addons/ares_zeusExtensions/config.cpp
+++ src/addons/ares_zeusExtensions/config.cpp
@@ -12,7 +12,7 @@ class CfgPatches
 		
 		units[] =
 		{
-			//"ares_ModuleTest"
+			"Ares_Module_Test"
 		};
 		
 		requiredAddons[] =
@@ -22,7 +22,7 @@ class CfgPatches
 		};
     };
 };
-/*
+
 class CfgFactionClasses
 {
 	class Ares // The section the module will show up under in the editor?? Or is this just whatever you want?
@@ -43,22 +43,69 @@ class CfgFactionClasses
 		};
 	};
 };
-*/
-/*
+
+
 class CfgVehicles
 {
 	class Logic;
 	class Module_F: Logic
 	{
+		class ArgumentsBaseUnits
+		{
+			class Units;
+		};
 		class ModuleDescription
 		{
-			class AnyPlayer;
 			class AnyBrain;
-			class EmptyDetector;
 		};
 	};
+	
+	class Ares_Module_Base : Module_F
+	{
+		mapSize = 0;
+		author = "Anton Struyk";
+		vehicleClass = "Modules";
+		category = "Curator";
+		subCategory = "Common";
+		side = 7;
+
+		scope = 1;				// Editor visibility; 2 will show it in the menu, 1 will hide it.
+		scopeCurator = 1;		// Curator visibility; 2 will show it in the menu, 1 will hide it.
+
+		displayName = "Ares Module Base";	// Name displayed in the menu
+		// icon = "\chessmastersCommonLibrary\data\icon_ca.paa";		// Map icon. Delete this entry to use the default icon
+		// picture = "\chessmastersCommonLibrary\data\icon_ca.paa";
+		// portrait = "\chessmastersCommonLibrary\data\icon_ca.paa";
+
+		function = "";			// Name of function triggered once conditions are met
+		functionPriority = 1;	// Execution priority, modules with lower number are executed first. 0 is used when the attribute is undefined
+		isGlobal = 2;			// 0 for server only execution, 1 for remote execution on all clients upon mission start, 2 for persistent execution
+		isTriggerActivated = 0;	// 1 for module waiting until all synced triggers are activated
+		isDisposable = 1;		// 1 if modules is to be disabled once it's activated (i.e., repeated trigger activation won't work)
+		// curatorInfoType = "RscDisplayAttributeModuleNuke";	// Menu displayed when the module is placed or double-clicked on by Zeus
+		
+		class Arguments : ArgumentsBaseUnits
+		{
+			class Units: Units {};
+		};
+		class ModuleDescription: ModuleDescription
+		{
+			description = "Ares Module Base";
+			sync[] = {};
+		};
+	};
+	
+	class Ares_Module_Test : Ares_Module_Base
+	{
+		scope = 2;
+		scopeCurator = 2;
+		//category = "Curator";
+		//subCategory = "Ares";
+		displayName = "Ares Test";
+		function = "Ares_fnc_test";
+	};
 };
-*/
+
 class CfgFunctions
 {
 	class Ares // This bit will be prefixed when actually calling the function (e.g. "Ares_fnc_...." )
