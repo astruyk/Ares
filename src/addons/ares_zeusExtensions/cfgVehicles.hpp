57452c51d9ac71ca163db61b12d827cc2624dece
Anton <Anton@.(none)>
2014-09-01 12:56:39 -0400

    Add stub functions for side-teleporting.

--- src/addons/ares_zeusExtensions/cfgVehicles.hpp
+++ src/addons/ares_zeusExtensions/cfgVehicles.hpp
@@ -25,3 +25,42 @@ class Ares_Module_RemoveNvgGroup : Ares_Module_Base
 	displayName = "Surrender (Unit)";
 	function = "Ares_fnc_SurrenderSingleUnit";
 };*/
+
+class Ares_Module_Teleport_All : Ares_Module_Base
+{
+	scopeCurator = 2;
+	displayName = "Teleport All";
+	function = "Ares_fnc_TeleportAllPlayers";
+};
+
+class Ares_Module_Teleport_Aaf : Ares_Module_Base
+{
+	scopeCurator = 2;
+	displayName = "Teleport AAF";
+	subCategory = "Teleport";
+	function = "Ares_fnc_TeleportAafPlayers";
+};
+
+class Ares_Module_Teleport_Civilian : Ares_Module_Base
+{
+	scopeCurator = 2;
+	displayName = "Teleport Civilians";
+	subCategory = "Teleport";
+	function = "Ares_fnc_TeleportCivilianPlayers";
+};
+
+class Ares_Module_Teleport_Csat : Ares_Module_Base
+{
+	scopeCurator = 2;
+	displayName = "Teleport CSAT";
+	subCategory = "Teleport";
+	function = "Ares_fnc_TeleportCsatPlayers";
+};
+
+class Ares_Module_Teleport_Nato : Ares_Module_Base
+{
+	scopeCurator = 2;
+	displayName = "Teleport NATO";
+	subCategory = "Teleport";
+	function = "Ares_fnc_TeleportNatoPlayers";
+};
--- src/addons/ares_zeusExtensions/units.hpp
+++ src/addons/ares_zeusExtensions/units.hpp
@@ -5,4 +5,9 @@ units[] =
 	"Ares_Module_RemoveNvgSingle",
 	"Ares_Module_RemoveNvgGroup",
 	//"Ares_Module_SurrenderSingleUnit",
+	"Ares_Module_Teleport_All",
+	"Ares_Module_Teleport_Aaf",
+	"Ares_Module_Teleport_Civilian",
+	"Ares_Module_Teleport_Csat",
+	"Ares_Module_Teleport_Nato",
 };
