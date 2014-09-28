85a9602dc39672419176a5c773ed42ef86d9ecd4
Anton <Anton@.(none)>
2014-09-27 22:40:13 -0400

    Update to V.0.0.2

--- bin/@Ares/mod.cpp
+++ bin/@Ares/mod.cpp
@@ -1,7 +1,7 @@
-name = "Ares 0.0.1";
+name = "Ares 0.0.2";
 picture = "ares_logo.paa";
 actionName = "Website";
 action = "https://github.com/astruyk/Ares";
-tooltip = "Ares 0.0.1";
+tooltip = "Ares 0.0.2";
 overview = "Ares augments the existing Zeus functionality, expanding the toolset and making it possible to create more compelling missions on the fly.";
 author = "Anton Struyk";
--- bin/@Ares/readme.txt
+++ bin/@Ares/readme.txt
@@ -1,7 +1,10 @@
-Ares V.0.0.1 
+Ares V.0.0.2
 Check out the documentation at: https://github.com/astruyk/Ares/wiki
 
 Changelists
+
+V.0.0.1
+	* Added Arsenal functions
 
 V.0.0.1
 	* Initial alpha release.
