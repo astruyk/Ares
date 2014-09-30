324d022ffca84c7b41c3142c697fbb92f8014961
Anton <Anton@.(none)>
2014-09-29 20:43:53 -0400

    Better name for absolute/relative paste.

--- src/addons/ares_zeusExtensions/cfgVehiclesModulesSaveLoad.hpp
+++ src/addons/ares_zeusExtensions/cfgVehiclesModulesSaveLoad.hpp
@@ -1,27 +1,27 @@
 class Ares_Module_Save_Objects_For_Composition : Ares_SaveLoad_Module_Base
 {
 	scopeCurator = 2;
-	displayName = "Save For Comp.";
+	displayName = "(DBG) Save For Comp.";
 	function = "Ares_fnc_SaveObjectsForComposition";
 };
 
 class Ares_Module_Save_Objects_For_Paste : Ares_SaveLoad_Module_Base
 {
 	scopeCurator = 2;
-	displayName = "Copy Nearby";
+	displayName = "Copy To Clipboard";
 	function = "Ares_fnc_SaveObjectsForPaste";
 };
 
 class Ares_Module_Paste_Objects_Relative : Ares_SaveLoad_Module_Base
 {
 	scopeCurator = 2;
-	displayName = "Paste Relative";
+	displayName = "Paste At New Position";
 	function = "Ares_fnc_PasteObjectsRelative";
 };
 
 class Ares_Module_Paste_Objects_Absolute: Ares_SaveLoad_Module_Base
 {
 	scopeCurator = 2;
-	displayName = "Paste Absolute";
+	displayName = "Paste At Original Position";
 	function = "Ares_fnc_PasteObjectsAbsolute";
 };
