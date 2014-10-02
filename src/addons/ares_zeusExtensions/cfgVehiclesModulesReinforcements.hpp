d504f875e57b7bf919d19e26abf701c44979b534
Anton <Anton@.(none)>
2014-10-02 19:19:45 -0400

    Use new icons for reinforcements & LZ's.

--- src/addons/ares_zeusExtensions/cfgVehiclesModulesReinforcements.hpp
+++ src/addons/ares_zeusExtensions/cfgVehiclesModulesReinforcements.hpp
@@ -3,6 +3,9 @@ class Ares_Module_Reinforcements_Create_Lz : Ares_Reinforcements_Module_Base
 	scopeCurator = 2;
 	displayName = "Create New LZ";
 	function = "Ares_fnc_ReinforcementsCreateLz";
+	icon = "\ares_zeusExtensions\data\icon_lz.paa";
+	picture = "\ares_zeusExtensions\data\icon_lz.paa";
+	portrait = "\ares_zeusExtensions\data\icon_lz.paa";
 };
 
 class Ares_Module_Reinforcements_Create_Rp : Ares_Reinforcements_Module_Base
@@ -10,6 +13,9 @@ class Ares_Module_Reinforcements_Create_Rp : Ares_Reinforcements_Module_Base
 	scopeCurator = 2;
 	displayName = "Create New RP";
 	function = "Ares_fnc_ReinforcementsCreateRp";
+	icon = "\ares_zeusExtensions\data\icon_rp.paa";
+	picture = "\ares_zeusExtensions\data\icon_rp.paa";
+	portrait = "\ares_zeusExtensions\data\icon_rp.paa";
 };
 
 class Ares_Module_Reinforcements_Spawn_Csat_Truck : Ares_Reinforcements_Module_Base
@@ -17,5 +23,6 @@ class Ares_Module_Reinforcements_Spawn_Csat_Truck : Ares_Reinforcements_Module_B
 	scopeCurator = 2;
 	displayName = "Spawn Units";
 	function = "Ares_fnc_ReinforcementsCreateUnits";
+	
 };
 
