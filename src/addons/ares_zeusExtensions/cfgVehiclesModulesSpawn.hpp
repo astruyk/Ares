71ea7b50974a1ba1a46ebee6bd21a0c890a682d3
Anton <Anton@.(none)>
2014-10-19 17:41:40 -0400

    Shorten names so they're not clipped.

--- src/addons/ares_zeusExtensions/cfgVehiclesModulesSpawn.hpp
+++ src/addons/ares_zeusExtensions/cfgVehiclesModulesSpawn.hpp
@@ -1,13 +1,13 @@
 class Ares_Module_Spawn_Smoke : Ares_Spawn_Module_Base
 {
 	scopeCurator = 2;
-	displayName = "Spawn Persistant Smoke Pillar";
+	displayName = "Persistent Smoke Pillar";
 	function = "Ares_fnc_SpawnSmoke";
 };
 
 class Ares_Module_Spawn_Custom_Mission_Objects : Ares_Spawn_Module_Base
 {
 	scopeCurator = 2;
-	displayName = "Spawn Custom Map Objects";
+	displayName = "Custom Mission Objects";
 	function = "Ares_fnc_SpawnCustomMissionObjects";
 };
