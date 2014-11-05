fb88502c3e4a969184681aff284e4ccbc3d30eaa
anton <astruyk@gmail.com>
2014-11-05 09:03:09 -0500

    Add scrip to generate PBO file.

---
+++ scripts/build_pbo.bat
@@ -0,0 +1,5 @@
+SET TOOL_DIR="C:\Games\Steam\steamapps\common\Arma 3 Tools\"
+SET CONTENT_DIR="C:\Users\Anton\Documents\SourceTree\Ares"
+
+%TOOL_DIR%\AddonBuilder\AddonBuilder.exe %CONTENT_DIR%\src\addons\ares_zeusExtensions %CONTENT_DIR%\bin\@Ares\addons -sign="..\..\..\..\Dropbox\Arma3\jtf2.biprivatekey" -packonly -clear > build_PBO_out.txt
+pause
