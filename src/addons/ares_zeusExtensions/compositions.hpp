9bc01ce95dd8bb54c113993ab283922f70ca99ea
anton <astruyk@gmail.com>
2014-12-02 14:14:15 -0500

    Fix some height issues with the junk wall.

--- src/addons/ares_zeusExtensions/compositions.hpp
+++ src/addons/ares_zeusExtensions/compositions.hpp
@@ -815,13 +815,12 @@ class AresCompositions
 			class AresRoadblockJunkVerySmall
 			{
 				name = "Junk";
-				class Object1 {side=8;vehicle="Land_IronPipes_F";rank="";position[]={0.361084,1.26563,-0.00143862};dir=0;};
-				class Object2 {side=8;vehicle="Land_Cages_F";rank="";position[]={-3.0564,1.57959,0.0143638};dir=267.511;};
-				class Object3 {side=8;vehicle="Land_Tyres_F";rank="";position[]={-3.33789,1.59619,0.0051589};dir=0;};
-				class Object4 {side=8;vehicle="Land_Sacks_heap_F";rank="";position[]={3.82861,1.61719,-0.00143862};dir=0;};
-				class Object5 {side=8;vehicle="Land_Pallets_stack_F";rank="";position[]={3.81396,1.73438,-0.00143862};dir=92.0786;};
-				class Object6 {side=8;vehicle="Land_CratesPlastic_F";rank="";position[]={5.03931,1.44678,-0.00143862};dir=0;};
-				class Object7 {side=8;vehicle="Land_Bricks_V4_F";rank="";position[]={5.15869,1.55225,-0.00143862};dir=0.00103247;};
+				class Object0 {side=8;rank="";vehicle="Land_CncBarrier_F";position[]={0.765137,0.399902,0};dir=0;};
+				class Object1 {side=8;rank="";vehicle="Land_CncBarrier_F";position[]={0.82666,1.07227,0};dir=0;};
+				class Object2 {side=8;rank="";vehicle="Land_IronPipes_F";position[]={-2.30371,0.57666,0};dir=0;};
+				class Object3 {side=8;rank="";vehicle="Land_CratesWooden_F";position[]={3.20166,0.620605,0};dir=0;};
+				class Object4 {side=8;rank="";vehicle="Land_Cages_F";position[]={-5.82617,0.944336,0};dir=267;};
+				class Object5 {side=8;rank="";vehicle="Land_Tyres_F";position[]={-6.00293,0.907227,0};dir=0;};
 			};
 
 			class AresRoadblockCivilianWrecks
