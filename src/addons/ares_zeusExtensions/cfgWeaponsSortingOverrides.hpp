ab1a5c61a6dd04644fd9c7f5cae2f38036a258e7
anton <astruyk@gmail.com>
2014-12-23 16:48:17 -0500

    Set side for weapons so they can be sorted into boxes correctly.

--- src/addons/ares_zeusExtensions/cfgWeaponsSortingOverrides.hpp
+++ src/addons/ares_zeusExtensions/cfgWeaponsSortingOverrides.hpp
@@ -19,6 +19,9 @@ class H_Booniehat_khk;
 class H_MilCap_oucamo;
 class Vest_Camo_Base;
 class Vest_NoCamo_Base;
+class Rifle_Base_F;
+class Launcher_Base_F;
+class Rifle_Long_Base_F;
 
 // CSAT - but has to be defined first because some other hats depend on it
 class H_MilCap_ocamo : H_HelmetB { side = SIDE_CSAT; }
@@ -56,7 +59,8 @@ class V_PlateCarrierSpec_rgr : Vest_NoCamo_Base { side = SIDE_NATO; }
 class V_RebreatherB : Vest_Camo_Base { side = SIDE_NATO; }
 class V_I_G_resistanceLeader_F : Vest_Camo_Base { side = SIDE_NATO; }
 // Weapons
-//...
+class arifle_MX_Base_F : Rifle_Base_F { side = SIDE_NATO; }
+class EBR_base_F : Rifle_Long_Base_F { side = SIDE_NATO; }
 
 // OPFOR
 // Headgear
@@ -71,6 +75,12 @@ class H_PilotHelmetHeli_O : H_PilotHelmetHeli_B { side = SIDE_CSAT; }
 class V_HarnessO_brn : Vest_Camo_Base { side = SIDE_CSAT; }
 class V_HarnessOGL_brn : Vest_Camo_Base { side = SIDE_CSAT; }
 class V_RebreatherIR : V_RebreatherB { side = SIDE_CSAT; }
+// Weapons
+class arifle_Katiba_Base_F : Rifle_Base_F { side = SIDE_CSAT; }
+class launch_RPG32_F : Launcher_Base_F { side = SIDE_CSAT; }
+class LMG_Zafir_F : Rifle_Long_Base_F { side = SIDE_CSAT; }
+class DMR_01_base_F : Rifle_Long_Base_F { side = SIDE_CSAT; }
+class GM6_base_F : Rifle_Long_Base_F { side = SIDE_CSAT; }
 
 // AAF
 // Headgear
@@ -85,7 +95,11 @@ class H_PilotHelmetHeli_I : H_PilotHelmetHeli_B { side = SIDE_AAF; }
 // Vests
 class V_PlateCarrierIA1_dgtl : Vest_Camo_Base { side = SIDE_AAF; }
 class V_RebreatherIA : V_RebreatherB { side = SIDE_AAF; }
-
+// Weapons
+class mk20_base_F : Rifle_Base_F { side = SIDE_AAF; }
+class Tavor_base_F : Rifle_Base_F { side = SIDE_AAF; }
+class LMG_Mk200_F : Rifle_Long_Base_F { side = SIDE_AAF; }
+class LRR_base_F : Rifle_Long_Base_F { side = SIDE_NATO; }
 
 // CIVILIAN
 // Headgear
